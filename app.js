eab75e3fbd21dcd2c34b1ef9fd78f0faedd99a70
kickpoint083 <aumpeerapol@gmail.com>
2022-07-20 11:47:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('GitHub')
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'))
+
+app.get("/", (req,res) =>{
+    res.send('Hello World');
+})
+
+app.listen(port, () => {
+    debug(chalk.green("Listening on port" + port));
+})
